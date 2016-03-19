ea4e5b6f2bef8cdde2601f5716a5d3da511cff6d
dschonholtz <schonholtzd@gmail.com>
2016-03-19 17:38:42 -0400

    More View Things

--- src/cs3500/music/MusicEditor.java
+++ src/cs3500/music/MusicEditor.java
@@ -4,6 +4,7 @@ import cs3500.music.model.GenericSong;
 import cs3500.music.util.CompositionBuilder;
 import cs3500.music.util.MusicReader;
 import cs3500.music.view.ConsoleView;
+import cs3500.music.view.GuiViewFrame;
 //import cs3500.music.view.GuiViewFrame;
 //import cs3500.music.view.MidiViewImpl;
 
@@ -14,13 +15,18 @@ import javax.sound.midi.InvalidMidiDataException;
 
 public class MusicEditor {
     public static void main(String[] args) throws IOException, InvalidMidiDataException {
-//        GuiViewFrame view = new GuiViewFrame();
-//        MidiViewImpl midiView = new MidiViewImpl();
         MusicReader mr = new MusicReader();
         CompositionBuilder<GenericSong> cb = new GenericSong.Builder();
 
-        ConsoleView test1 = new ConsoleView(mr.parseFile(new FileReader("C:\\Users\\Ari\\Dropbox\\Code\\Java\\MusicEditor\\src\\mystery-1.txt"), cb));
-        test1.run();
+        //ConsoleView test1 = new ConsoleView(mr.parseFile(new FileReader("C:\\Users\\duggy_000\\IdeaProjects\\cs3500\\MusicEditor\\src\\mystery-3.txt"), cb));
+        //test1.run();
+
+        GuiViewFrame view = new GuiViewFrame(mr.parseFile(new FileReader("C:\\Users\\duggy_000\\IdeaProjects\\cs3500\\MusicEditor\\src\\mary-little-lamb.txt"), cb));
+        view.run();
+
+//      MidiViewImpl midiView = new MidiViewImpl();
+
+
 //         You probably need to connect these views to your model, too...
     }
 }
