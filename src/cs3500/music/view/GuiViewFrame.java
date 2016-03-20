363a8cf06d230ea85ded879b64b7413c7c7c2cfc
Ari <arianna2028@outlook.com>
2016-03-20 10:39:41 -0400

    Added a model field to guiviewframe

--- src/cs3500/music/view/GuiViewFrame.java
+++ src/cs3500/music/view/GuiViewFrame.java
@@ -1,5 +1,6 @@
 package cs3500.music.view;
 
+import cs3500.music.model.GenericSong;
 import cs3500.music.model.SongRep;
 
 import java.awt.*;
@@ -11,8 +12,8 @@ import javax.swing.*;
  * A skeleton Frame (i.e., a window) in Swing
  */
 public class GuiViewFrame extends javax.swing.JFrame implements IMusicView {
-
     private final JPanel displayPanel; // You may want to refine this to a subtype of JPanel
+    private final SongRep model;
 
     /**
      * Creates new GuiView
@@ -22,13 +23,15 @@ public class GuiViewFrame extends javax.swing.JFrame implements IMusicView {
         this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         this.getContentPane().add(displayPanel);
         this.pack();
+        this.model = new GenericSong(); //TODO what else could we initialize this as?
     }
 
-    public GuiViewFrame(SongRep song) {
+    public GuiViewFrame(SongRep model) {
         this.displayPanel = new ConcreteGuiViewPanel();
         this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         this.getContentPane().add(displayPanel);
         this.pack();
+        this.model = model;
     }
 
     public void initialize(){
