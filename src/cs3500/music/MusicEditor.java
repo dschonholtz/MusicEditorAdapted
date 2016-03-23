56e548eae23a808ff46cd2ecaab0360c64a8339b
dschonholtz <schonholtzd@gmail.com>
2016-03-23 12:28:43 -0400

    EVERYTHING STILL SORTA WORKS

--- src/cs3500/music/MusicEditor.java
+++ src/cs3500/music/MusicEditor.java
@@ -11,7 +11,7 @@ public class MusicEditor {
             throw new IOException("You havfta have at least two command line arrrggs");
         }
         IMusicView view = new ViewFactory().buildView(args[0], args[1]);
-        IMusicView view2 = new ViewFactory().buildView(args[0], args[2]);
+        IMusicView view2 = new ViewFactory().buildView(args[0], "midi");
         view.run();
         view2.run();
 //        IMusicView testView = new ViewFactory().buildView("mary-little-lamb.txt", "test");
--- tests/ViewTest.java
+++ tests/ViewTest.java
@@ -1,13 +1,34 @@
+import cs3500.music.model.GenericSong;
 import cs3500.music.model.Note;
+import cs3500.music.model.Pitch;
+import cs3500.music.model.SongRep;
 import cs3500.music.view.ConcreteGuiViewPanel;
+import org.junit.Test;
 
 import javax.swing.*;
+
+import java.awt.*;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by dschonholtz on 3/22/2016.
  */
 public class ViewTest {
     //Do we want to build everything through the factory for testing or no?
-    JPanel guiView = new ConcreteGuiViewPanel();
-    Note n = new Note(); // c4
+
+    @Test
+    public void testGetPreferedSize() {
+        SongRep gs1 = new GenericSong();
+        gs1.addNote(new Note());
+        gs1.addNote(new Note(5, 4, 3,Pitch.C, 3, 65));
+        gs1.addNote(new Note(5, 4, 3, Pitch.D, 3, 65));
+        JPanel guiView = new ConcreteGuiViewPanel(gs1);
+
+        assertEquals(guiView.getPreferredSize(),new Dimension(200,340));
+
+        gs1.addNote(new Note(10, 50, 1, Pitch.D, 3, 65));
+        guiView = new ConcreteGuiViewPanel(gs1);                       //TODO: THIS LINE IS NECESSARY THEY ARE NOT CONNECTED BY REFERENCE... WHY????
+        assertEquals(guiView.getPreferredSize(),new Dimension(1280,780));
+    }
 }
