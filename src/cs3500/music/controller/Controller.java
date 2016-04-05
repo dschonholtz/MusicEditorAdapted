4f935c304bb69193a5543b0f5395eba30669a157
Ari <arianna2028@outlook.com>
2016-04-04 23:01:45 -0400

    Can now hold shift to move the currentbeat forward or backward by one measure instead of moving just the screen

--- src/cs3500/music/controller/Controller.java
+++ src/cs3500/music/controller/Controller.java
@@ -22,11 +22,13 @@ public class Controller implements IController {
     private final SongRep model;
     private final CompositeView view;
     private boolean playing;
+    private boolean holdingShift;
 
     public Controller(SongRep model) {
         this.model = model;
         this.view = new CompositeView(model);
         this.playing = true;
+        this.holdingShift = false;
         setUpKeys();
     }
 
@@ -63,6 +65,8 @@ public class Controller implements IController {
 
     private void setUpKeys() {
         Map<Integer, Runnable> keyPresses = new HashMap<>();
+        Map<Integer, Runnable> keyReleases = new HashMap<>();
+
         keyPresses.put(KeyEvent.VK_SPACE, new Pause());
         keyPresses.put(KeyEvent.VK_DOWN, new ScrollDown());
         keyPresses.put(KeyEvent.VK_UP, new ScrollUp());
@@ -70,9 +74,13 @@ public class Controller implements IController {
         keyPresses.put(KeyEvent.VK_RIGHT, new ScrollRight());
         keyPresses.put(KeyEvent.VK_HOME, new SkipToStart());
         keyPresses.put(KeyEvent.VK_END, new SkipToEnd());
+        keyPresses.put(KeyEvent.VK_SHIFT, new HoldShift());
+
+        keyReleases.put(KeyEvent.VK_SHIFT, new ReleaseShift());
 
         KeyboardHandler kh = new KeyboardHandler();
         kh.setKeyPressedMap(keyPresses);
+        kh.setKeyReleasedMap(keyReleases);
         view.addKeyListener(kh);
     }
 
@@ -94,11 +102,23 @@ public class Controller implements IController {
     }
 
     class ScrollLeft implements Runnable { //TODO note: if scrolling left of red line, will scroll you back automatically
-        public void run() { view.scrollLeft(); }
+        public void run() {
+            view.scrollLeft();
+
+            if (holdingShift) {
+                model.setCurrentBeat(Math.max(0, model.getBeat() - 4));
+            }
+        }
     }
 
     class ScrollRight implements Runnable {
-        public void run() { view.scrollRight(); }
+        public void run() {
+            view.scrollRight();
+
+            if (holdingShift) {
+                model.setCurrentBeat(model.getBeat() + 4);
+            }
+        }
     }
 
     class SkipToStart implements Runnable {
@@ -107,5 +127,13 @@ public class Controller implements IController {
 
     class SkipToEnd implements Runnable {
         public void run() { jumpTo(model.getLength()); }
+    }
+
+    class HoldShift implements Runnable {
+        public void run() { holdingShift = true; }
+    }
+
+    class ReleaseShift implements Runnable {
+        public void run() { holdingShift = false; }
     }
 }
