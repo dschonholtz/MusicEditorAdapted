b1bd4fbf537690e0701fd377fb6cda8ddcfbe544
Ari <arianna2028@outlook.com>
2016-04-05 10:00:53 -0400

    Added javaDoc

--- src/cs3500/music/view/GuiView.java
+++ src/cs3500/music/view/GuiView.java
@@ -8,41 +8,60 @@ import java.awt.event.MouseListener;
 
 public interface GuiView extends IMusicView {
     /**
-     * Scroll left
+     * Scroll the view left
      */
     void scrollLeft();
 
     /**
-     * Scroll right
+     * Scroll the view right
      */
     void scrollRight();
 
     /**
-     * Scroll up
+     * Scroll the view up
      */
     void scrollUp();
 
     /**
-     * Scroll down
+     * Scroll the view down
      */
     void scrollDown();
 
     /**
-     * @return - This is the note at the given location regardless of whether or not there is actually a note there
-     * now
+     * @return - This is the note at the given location regardless of whether there is one
      */
     NoteRep getNoteAtMouseLocation(Point location);
 
     /**
-     * THis returns true if there is a note at the given location
+     * @return true if there is a note at the given location
      */
     boolean noteAtLocation(Point location);
 
+    /**
+     * Flips the play state from paused to playing or vice-versa
+     */
     void changePlayState();
 
+    /**
+     * Adds a {@link KeyListener} with functionality to the view
+     * @param keyListener the {@link KeyListener} to add to the view
+     */
     void addKeyListener(KeyListener keyListener);
+
+    /**
+     * Adds a {@link MouseListener} with functionality to the view
+     * @param mouseListener {@link MouseListener} to add to the view
+     */
     void addMouseListener(MouseListener mouseListener);
 
+    /**
+     * Jumps the view to a specific beat in the song
+     * @param beat the beat to jump to
+     */
     void jumpTo(int beat);
+
+    /**
+     * @return the location of the mouse as a {@link Point}
+     */
     Point getMousePosition();
 }
